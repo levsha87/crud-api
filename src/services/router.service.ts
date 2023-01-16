1d47b0411c82815971dd6ca6ee2559a2918a90d7
Raman Liausha <Raman_Liausha@epam.com>
2023-01-16 23:46:23 +0300

    feat: add logic for delete user

--- src/services/router.service.ts
+++ src/services/router.service.ts
@@ -1,4 +1,4 @@
-import { users, createUser, getUserById, updateUser } from '../db.js';
+import { users, createUser, getUserById, updateUser, deleteUser } from '../db.js';
 import http from 'http';
 import { getRequestData } from '../helpers/getRequestData.js';
 
@@ -10,7 +10,7 @@ export async function router(request: http.IncomingMessage, response: http.Serve
                 response.end(JSON.stringify({
                     data: users,
                 }));
-                return;
+                break;
             case 'POST':
                 try {
                     const postData = JSON.parse(await getRequestData(request));
@@ -19,25 +19,27 @@ export async function router(request: http.IncomingMessage, response: http.Serve
                     response.end();
                 } catch (err) {
                     const error = err as Error;
-                    response.statusCode = 400;
+                    response.statusCode = setErrorCode(error.message);
                     response.end(JSON.stringify({ message: error.message }));
                 }
-                return;
+                break;
         }
     } else if (request.url?.startsWith('/api/users/') && request.url?.split('/')[2]) {
         switch (request.method) {
             case 'GET':
                 try {
-                    response.writeHead(200, { 'Content-Type': 'application/json' });
+                    const user = getUserById(request.url?.split('/')[3]);
+                    response.statusCode = 200;
+                    response.setHeader('Content-Type', 'application/json');
                     response.end(JSON.stringify({
-                        data: getUserById(request.url?.split('/')[3]),
+                        data: user,
                     }));
                 } catch (err) {
                     const error = err as Error;
-                    response.statusCode = 404;
+                    response.statusCode = setErrorCode(error.message);
                     response.end(JSON.stringify({ message: error.message }));
                 }
-                return;
+                break;
             case 'PUT':
                 try {
                     const postData = await getRequestData(request);
@@ -46,26 +48,35 @@ export async function router(request: http.IncomingMessage, response: http.Serve
                     response.end();
                 } catch (err) {
                     const error = err as Error;
-                    response.statusCode = 400;
+                    response.statusCode = setErrorCode(error.message);
                     response.end(JSON.stringify({ message: error.message }));
                 }
-                return;
-            // case 'DELETE':
-            //     try {
-            //         const postData = JSON.parse(await getRequestData(request));
-            //         createUser(postData.username, postData.age, postData.hobbies);
-            //         response.statusCode = 201;
-            //         response.end();
-            //     } catch (err) {
-            //         const error = err as Error;
-            //         response.statusCode = 400;
-            //         response.end(JSON.stringify({ message: error.message }));
-            //     }
-            //     return;
+                break;
+            case 'DELETE':
+                try {
+                    deleteUser(request.url?.split('/')[3]);
+                    response.statusCode = 204;
+                    response.end();
+                } catch (err) {
+                    const error = err as Error;
+                    response.statusCode = setErrorCode(error.message);
+                    response.end(JSON.stringify({ message: error.message }));
+                }
+                break;
         }
     } else {
         response.statusCode = 404
         response.write("Page not found. Please, check that the information you entered is correct.")
         response.end()
     }
+}
+
+function setErrorCode(msg:string) {
+    switch (msg) {
+        case 'UserId is invalid':
+            return 400;
+        case "Record with id === userId doesn't exist":
+            return 404;
+    }
+    return 500;
 }
